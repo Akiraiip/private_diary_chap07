5fc2bafe401012822e858f5888c4ac69809af01c
Akiraiip <iip.git.akira.watanabe@gmail.com>
2023-04-26 11:54:00 +0900

    iip jQuery progress bar has running!

--- static/js/4-1-5.js
+++ static/js/4-1-5.js
@@ -1,6 +1,6 @@
 //SVGアニメーションの描画
-var stroke;
-stroke = new Vivus('mask', {//アニメーションをするIDの指定
+//var stroke;
+let stroke = new Vivus('mask', {//アニメーションをするIDの指定
     start:'manual',//自動再生をせずスタートをマニュアルに
     type: 'scenario-sync',// アニメーションのタイプを設定
     duration: 10,//アニメーションの時間設定。数字が小さくなるほど速い
