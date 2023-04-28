3c706b335dfac7198a72f0f1d6893ef7ca54e1e4
akira <iti.akira.watanabe@gmail.com>
2023-04-28 19:18:00 +0900

    bokash runned

--- static/js/5-1-25.js
+++ static/js/5-1-25.js
@@ -1,11 +1,12 @@
-$(".openbtn").click(function () {//ボタンがクリックされたら
+//$(".openbtn").click(function () {//ボタンがクリックされたら
+$(".globalBtn").click(function () {//ボタンがクリックされたら
 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
     $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-    $("#header,#container").toggleClass('mainblur');//ぼかしたいエリアにmainblurクラスを付与
+    $("#header,#home").toggleClass('mainblur');//ぼかしたいエリアにmainblurクラスを付与
 });
 
 $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $(".globalBtn").removeClass('active');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去し
-    $("#header,#container").removeClass('mainblur');//ぼかしているエリアのmainblurクラスを除去
+    $("#header,#home").removeClass('mainblur');//ぼかしているエリアのmainblurクラスを除去
 });
