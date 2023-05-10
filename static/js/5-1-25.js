01553681b6dc74419e8d68470696fadd84474f8a
Akiraiip <iip.git.akira.watanabe@gmail.com>
2023-05-10 11:00:22 +0900

    openbtnの完成

--- static/js/5-1-25.js
+++ static/js/5-1-25.js
@@ -20,32 +20,64 @@
 ////    $("#globalBtn").removeClass('panelBtn');//ナビゲーションのpanelBtnクラスを除去し
 //    $("#header,#home").removeClass('mainblur');//ぼかしているエリアのmainblurクラスを除去
 //});
-$("#g-nav a").click(function () {
-    $(".openbtn").removeClass('active');
-    $(".openbtn").removeClass('is-open');
-    $("#g-nav").removeClass('panelactive');
-    $("#header,#home").removeClass('mainblur');
-});
-
-$("#g-nav-list li:first-child a").click(function () {
-    $(".openbtn").removeClass('active');
-    $(".openbtn").removeClass('is-open');
-    $("#g-nav").removeClass('panelactive');
-    $("#header,#home").removeClass('mainblur');
-});
-$(".openbtn").click(function () {
-    $(this).toggleClass('active');
-    $(this).toggleClass('is-open');
-    $("#g-nav").toggleClass('panelactive');
-    $("#header,#home").toggleClass('mainblur');
-});
-
 //$("#g-nav a").click(function () {
 //    $(".openbtn").removeClass('active');
 //    $(".openbtn").removeClass('is-open');
 //    $("#g-nav").removeClass('panelactive');
 //    $("#header,#home").removeClass('mainblur');
 //});
+//
+//$("#g-nav-list li:first-child a").click(function () {
+//    $(".openbtn").removeClass('active');
+//    $(".openbtn").removeClass('is-open');
+//    $("#g-nav").removeClass('panelactive');
+//    $("#header,#home").removeClass('mainblur');
+//});
+//$(".openbtn").click(function () {
+//    $(this).toggleClass('active');
+////    $(this).toggleClass('is-open');
+//    $("#g-nav").toggleClass('panelactive');
+//    $("#home").toggleClass('mainblur');
+//    $("#header,#home").toggleClass('mainblur');
+//});
+//
+//$("#g-nav a").click(function () {
+//    $(".openbtn").removeClass('active');
+////    $(".openbtn").removeClass('is-open');
+//    $("#g-nav").removeClass('panelactive');
+//    $("#home").removeClass('mainblur');
+//    $("#header,#home").removeClass('mainblur');
+//});
+
+//$(document).on('click', '.openbtn', function() {
+//  $(this).toggleClass('active');
+//  $(this).toggleClass('is-open');
+//  $("#g-nav").toggleClass('panelactive');
+//  $("#header,#home").toggleClass('mainblur');
+//});
+//
+//$(document).on('click', '#g-nav a', function() {
+//  $(".openbtn").removeClass('active');
+//  $(".openbtn").removeClass('is-open');
+//  $("#g-nav").removeClass('panelactive');
+//  $("#header,#home").removeClass('mainblur');
+//});
+$(".openbtn").click(function(event) {
+  event.stopPropagation(); // イベント伝播の停止
+  $(this).toggleClass('active');
+  $(this).toggleClass('is-open');
+  $("#g-nav").toggleClass('panelactive');
+  $("#header,#home").toggleClass('mainblur');
+});
+
+$("#g-nav a").click(function(event) {
+  event.stopPropagation(); // イベント伝播の停止
+  $(".openbtn").removeClass('active');
+  $(".openbtn").removeClass('is-open');
+  $("#g-nav").removeClass('panelactive');
+  $("#header,#home").removeClass('mainblur');
+});
+
 
 
 
